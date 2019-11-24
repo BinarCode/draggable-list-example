d902bb9ec5d68238211cafe03129d8b0c286e0ec
cristij <joracristi@gmail.com>
2019-11-24 19:23:47 +0200

    Extract user card component

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,12 @@
                    ghost-class="moving-card"
                    :list="users"
                    :animation="200">
-            <li v-for="user in users"
-                :key="user.id"
-                class="p-4 mb-3 flex justify-between items-center bg-white shadow rounded-lg cursor-move">
-                <div class="flex items-center">
-                    <img class="w-10 h-10 rounded-full" :src="user.avatar" :alt="user.name">
-                    <p class="ml-2 text-gray-700 font-semibold font-sans tracking-wide">{{user.name}}</p>
-                </div>
-                <div class="flex">
-                    <button aria-label="Edit user"
-                            class="p-1 focus:outline-none focus:shadow-outline text-teal-500 hover:text-teal-600"
-                            @click="onEdit(user)">
-                        <EditIcon/>
-                    </button>
-                    <button aria-label="Delete user"
-                            class="p-1 focus:outline-none focus:shadow-outline text-red-500 hover:text-red-600"
-                            @click="onDelete(user)">
-                        <Trash2Icon/>
-                    </button>
-                </div>
-            </li>
+            <user-card v-for="user in users"
+                       :user="user"
+                       :key="user.id"
+                       @on-edit="onEdit"
+                       @on-delete="onDelete">
+            </user-card>
         </draggable>
     </div>
 </template>
@@ -32,12 +18,13 @@
 <script>
   import { EditIcon, Trash2Icon } from "vue-feather-icons";
   import Draggable from 'vuedraggable'
-
+  import UserCard from "./components/UserCard";
   export default {
     name: "App",
     components: {
       Draggable,
       EditIcon,
+      UserCard,
       Trash2Icon
     },
     data() {
---
+++ src/components/UserCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <li class="p-4 mb-3 flex justify-between items-center bg-white shadow rounded-lg cursor-move">
+        <div class="flex items-center">
+            <img class="w-10 h-10 rounded-full" :src="user.avatar" :alt="user.name">
+            <p class="ml-2 text-gray-700 font-semibold font-sans tracking-wide">{{user.name}}</p>
+        </div>
+        <div class="flex">
+            <button aria-label="Edit user"
+                    class="p-1 focus:outline-none focus:shadow-outline text-teal-500 hover:text-teal-600"
+                    @click="$emit('on-edit', user)">
+                <EditIcon/>
+            </button>
+            <button aria-label="Delete user"
+                    class="p-1 focus:outline-none focus:shadow-outline text-red-500 hover:text-red-600"
+                    @click="$emit('on-delete', user)">
+                <Trash2Icon/>
+            </button>
+        </div>
+    </li>
+</template>
+<script>
+  export default {
+    props: {
+      user: {
+        type: Object,
+        default: () => ({})
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+<style>
+</style>
