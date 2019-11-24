3f2e9391a119737edbc4f1590738992d0a3e610e
cristij <joracristi@gmail.com>
2019-11-24 18:37:55 +0200

    Add vue draggable and initial drag support

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,40 @@
 <template>
     <div id="app" class="min-h-screen w-screen bg-gray-200 flex items-center justify-center">
         <ul class="w-full max-w-md">
-            <li v-for="user in users"
-                :key="user.id"
-                class="p-4 mb-3 flex justify-between items-center bg-white shadow rounded-lg cursor-move">
-                <div class="flex items-center">
-                    <img class="w-10 h-10 rounded-full" :src="user.avatar" :alt="user.name">
-                    <p class="ml-2 text-gray-700 font-semibold font-sans tracking-wide">{{user.name}}</p>
-                </div>
-                <div class="flex">
-                    <button aria-label="Edit user"
-                            class="p-1 focus:outline-none focus:shadow-outline text-teal-500 hover:text-teal-600"
-                            @click="onEdit(user)">
-                        <EditIcon/>
-                    </button>
-                    <button aria-label="Delete user"
-                            class="p-1 focus:outline-none focus:shadow-outline text-red-500 hover:text-red-600"
-                            @click="onDelete(user)">
-                        <Trash2Icon/>
-                    </button>
-                </div>
-            </li>
+            <draggable :list="users">
+                <li v-for="user in users"
+                    :key="user.id"
+                    class="p-4 mb-3 flex justify-between items-center bg-white shadow rounded-lg cursor-move">
+                    <div class="flex items-center">
+                        <img class="w-10 h-10 rounded-full" :src="user.avatar" :alt="user.name">
+                        <p class="ml-2 text-gray-700 font-semibold font-sans tracking-wide">{{user.name}}</p>
+                    </div>
+                    <div class="flex">
+                        <button aria-label="Edit user"
+                                class="p-1 focus:outline-none focus:shadow-outline text-teal-500 hover:text-teal-600"
+                                @click="onEdit(user)">
+                            <EditIcon/>
+                        </button>
+                        <button aria-label="Delete user"
+                                class="p-1 focus:outline-none focus:shadow-outline text-red-500 hover:text-red-600"
+                                @click="onDelete(user)">
+                            <Trash2Icon/>
+                        </button>
+                    </div>
+                </li>
+            </draggable>
         </ul>
     </div>
 </template>
 
 <script>
   import { EditIcon, Trash2Icon } from "vue-feather-icons";
+  import Draggable from 'vuedraggable'
 
   export default {
     name: "App",
     components: {
+      Draggable,
       EditIcon,
       Trash2Icon
     },
